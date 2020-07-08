16957f963bb60e62d7183c5a9b49a2a195d0318b
MateusHBR <mateusmorais.r@gmail.com>
2020-07-08 16:09:25 -0300

    página de pesquisa

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+import 'package:pesquisa_cep/app/modules/home/components/skeleton_component.dart';
+import 'package:pesquisa_cep/app/modules/home/components/text_component.dart';
+import 'package:pesquisa_cep/app/shared/models/cep_model.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,8 +21,22 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Busca CEP"),
+        actions: <Widget>[
+          Observer(
+            builder: (_) {
+              return IconButton(
+                icon: Icon(Icons.clear),
+                onPressed: controller.isEmpty ? null : controller.clearContext,
+              );
+            },
+          )
+        ],
+      ),
       body: Container(
         margin: EdgeInsets.all(7.5),
         width: double.infinity,
@@ -29,35 +48,39 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                 child: Column(
                   children: <Widget>[
                     TextFormField(
+                      textCapitalization: TextCapitalization.words,
+                      onChanged: controller.setCidade,
+                      controller: controller.cidadeController,
                       validator: (cidade) {
                         if (cidade.isEmpty || cidade.length < 3) {
                           return "campo inválido";
                         }
                         return null;
                       },
-                      controller: controller.cidadeController,
                       decoration: InputDecoration(
                         labelText: "Cidade",
                         hintText: "Informe a cidade a ser consultada",
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: TextFormField(
-                        validator: (rua) {
-                          if (rua.isEmpty || rua.length < 3) {
-                            return "campo inválido";
-                          }
-                          return null;
-                        },
-                        controller: controller.ruaController,
-                        decoration: InputDecoration(
-                          labelText: "Rua",
-                          hintText: "Informe a rua a ser consultada",
-                        ),
+                    TextFormField(
+                      textCapitalization: TextCapitalization.words,
+                      controller: controller.ruaController,
+                      onChanged: controller.setRua,
+                      validator: (rua) {
+                        if (rua.isEmpty || rua.length < 3) {
+                          return "campo inválido";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Rua",
+                        hintText: "Informe a rua a ser consultada",
                       ),
                     ),
                     TextFormField(
+                      textCapitalization: TextCapitalization.characters,
+                      controller: controller.ufController,
+                      onChanged: controller.setUf,
                       validator: (uf) {
                         if (uf.isEmpty || uf.length < 2) {
                           return "campo inválido";
@@ -65,7 +88,6 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                         return null;
                       },
                       maxLength: 2,
-                      controller: controller.ufController,
                       inputFormatters: [
                         LengthLimitingTextInputFormatter(2),
                       ],
@@ -74,26 +96,30 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                         hintText: "Informe o UF a ser consultado, Ex: RJ",
                       ),
                     ),
-                    Container(
-                      margin: EdgeInsets.only(top: 10),
-                      width: double.infinity,
-                      child: RaisedButton(
-                        child: Text(
-                          'Buscar',
-                          style: TextStyle(
-                            color: Colors.white,
-                            letterSpacing: 0.5,
+                    Observer(
+                      builder: (_) {
+                        return Container(
+                          margin: EdgeInsets.only(top: 2, bottom: 5),
+                          width: double.infinity,
+                          child: RaisedButton(
+                            child: Text(
+                              'Buscar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                letterSpacing: 0.5,
+                              ),
+                            ),
+                            color: Theme.of(context).primaryColor,
+                            onPressed: controller.canClick
+                                ? () {
+                                    if (_formKey.currentState.validate()) {
+                                      controller.fetchData();
+                                    }
+                                  }
+                                : null,
                           ),
-                        ),
-                        color: Theme.of(context).primaryColor,
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            print('sucesso');
-                          } else {
-                            print('fail');
-                          }
-                        },
-                      ),
+                        );
+                      },
                     ),
                   ],
                 ),
@@ -101,51 +127,67 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
             ),
             Expanded(
               child: Container(
-                child: ListView.separated(
-                  separatorBuilder: (context, index) => Divider(),
-                  itemCount: 100,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      // height: 80,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Icon(
-                            Icons.location_city,
-                            size: 80,
-                            color: Colors.grey,
+                child: Observer(
+                  builder: (_) {
+                    if (controller.cepFuture.error != null ||
+                        controller.cepFuture.status == FutureStatus.rejected) {
+                      return Container(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'Erro ao carregar dados...',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize:
+                                20 * MediaQuery.of(context).textScaleFactor,
                           ),
-                          Column(
-                            children: <Widget>[
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('TTeste'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
+                        ),
+                      );
+                    }
+
+                    if (controller.cepFuture.status == FutureStatus.pending) {
+                      return SkeletonComponent();
+                    }
+
+                    if (controller.cepFuture.value.isEmpty &&
+                        !controller.firstTime) {
+                      return Center(
+                        child: Text(
+                          'Nenhum dado com essas características foi encontrado...',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize:
+                                16 * MediaQuery.of(context).textScaleFactor,
+                          ),
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: controller.cepList.length,
+                      itemBuilder: (context, index) {
+                        List<CepModel> list = controller.cepList;
+
+                        return Card(
+                          elevation: 5,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.location_city,
+                                  size: size.height * 0.08,
+                                  color: Colors.grey,
+                                ),
+                                TextComponent(
+                                  item: list[index],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     );
                   },
                 ),
