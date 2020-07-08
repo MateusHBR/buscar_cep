4da00f842101e7225039df0d1008abd3d74b60fc
MateusHBR <mateusmorais.r@gmail.com>
2020-07-08 18:38:42 -0300

    adicionando splash screen ao app module

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -6,6 +6,7 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:pesquisa_cep/app/app_widget.dart';
 import 'package:pesquisa_cep/app/modules/home/home_module.dart';
+import './splash/splash_screen.dart';
 
 class AppModule extends MainModule {
   @override
@@ -16,7 +17,8 @@ class AppModule extends MainModule {
 
   @override
   List<Router> get routers => [
-        Router(Modular.initialRoute, module: HomeModule()),
+        Router(Modular.initialRoute, child: (context, args) => SplashScreen()),
+        Router("/home", module: HomeModule()),
       ];
 
   @override
