015ce0a11eddf07f1f7f9a52b967b08716232c4d
MateusHBR <mateusmorais.r@gmail.com>
2020-07-08 18:39:19 -0300

    adicionando observer para setar o tema

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pesquisa_cep/app/app_controller.dart';
 
 class AppWidget extends StatelessWidget {
+  final controller = Modular.get<AppController>();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      navigatorKey: Modular.navigatorKey,
-      title: 'Flutter Slidy',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      onGenerateRoute: Modular.generateRoute,
+    return Observer(
+      builder: (_) {
+        if (controller.themeData != null) {
+          return MaterialApp(
+            navigatorKey: Modular.navigatorKey,
+            title: 'Flutter Slidy',
+            debugShowCheckedModeBanner: false,
+            theme: controller.themeData,
+            initialRoute: '/',
+            onGenerateRoute: Modular.generateRoute,
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 }
