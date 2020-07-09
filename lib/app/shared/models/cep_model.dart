8799e40045920d8e772456d9f3385d0948ce1c6f
MateusHBR <mateusmorais.r@gmail.com>
2020-07-09 16:04:22 -0300

    métodos para conversão para utilizar no SharedPrefs

--- lib/app/shared/models/cep_model.dart
+++ lib/app/shared/models/cep_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class CepModel {
   String cep;
   String rua;
@@ -5,6 +7,7 @@ class CepModel {
   String bairro;
   String cidade;
   String uf;
+  bool favorite;
 
   CepModel({
     this.cep,
@@ -13,6 +16,7 @@ class CepModel {
     this.bairro,
     this.cidade,
     this.uf,
+    this.favorite = false,
   });
 
   CepModel.fromJson(Map<String, dynamic> json) {
@@ -22,5 +26,55 @@ class CepModel {
     bairro = json["bairro"];
     cidade = json["localidade"];
     uf = json["uf"];
+    favorite = json["favorite"] ?? false;
   }
+
+  copyWith({
+    String cep,
+    String rua,
+    String complemento,
+    String bairro,
+    String cidade,
+    String uf,
+    bool favorite,
+  }) {
+    return CepModel(
+      cep: cep ?? this.cep,
+      rua: rua ?? this.rua,
+      complemento: complemento ?? this.complemento,
+      bairro: bairro ?? this.bairro,
+      cidade: cidade ?? this.cidade,
+      uf: uf ?? this.uf,
+      favorite: favorite ?? this.favorite,
+    );
+  }
+
+  static Map<String, dynamic> toJson(CepModel cepModel) {
+    return {
+      "cep": cepModel.cep,
+      "logradouro": cepModel.rua,
+      "complemento": cepModel.complemento,
+      "bairro": cepModel.bairro,
+      "localidade": cepModel.cidade,
+      "uf": cepModel.uf,
+      "favorite": cepModel.favorite,
+    };
+  }
+
+  static String encodeCeps(List<CepModel> ceps) {
+    List<Map<String, dynamic>> list = [];
+
+    for (var cepJson in ceps) {
+      var cep = CepModel.toJson(cepJson);
+      list.add(cep);
+    }
+
+    return json.encode(list);
+  }
+
+  static List<CepModel> decodeCeps(String ceps) => ceps != null
+      ? (json.decode(ceps) as List<dynamic>)
+          .map<CepModel>((item) => CepModel.fromJson(item))
+          .toList()
+      : [];
 }
