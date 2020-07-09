184d4edd0c8003b5676c92627701730da60e952a
MateusHBR <mateusmorais.r@gmail.com>
2020-07-09 16:05:33 -0300

    arquivos de teste

---
+++ test/app/modules/start/pages/favorites/favorites_controller_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_modular/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:pesquisa_cep/app/modules/start/pages/favorites/favorites_controller.dart';
+import 'package:pesquisa_cep/app/modules/start/start_module.dart';
+
+void main() {
+  initModule(StartModule());
+  // FavoritesController favorites;
+  //
+  setUp(() {
+    //     favorites = StartModule.to.get<FavoritesController>();
+  });
+
+  group('FavoritesController Test', () {
+    //   test("First Test", () {
+    //     expect(favorites, isInstanceOf<FavoritesController>());
+    //   });
+
+    //   test("Set Value", () {
+    //     expect(favorites.value, equals(0));
+    //     favorites.increment();
+    //     expect(favorites.value, equals(1));
+    //   });
+  });
+}
